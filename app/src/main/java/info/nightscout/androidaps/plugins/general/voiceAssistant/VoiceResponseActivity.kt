6de3a37fad97ba7252761d135d5edc9142a92d18
Andrew Warrington <awarring000@yahoo.com>
2021-02-02 11:02:38 +0000

    Testing of VoiceAssistantActivity.kt

--- app/src/main/java/info/nightscout/androidaps/plugins/general/voiceAssistant/VoiceResponseActivity.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/voiceAssistant/VoiceResponseActivity.kt
@@ -16,12 +16,11 @@ class VoiceResponseActivity : NoSplashAppCompatActivity() {
         //external voice assistant must implement a receiver to speak these messages back to the user.
         //this is possible via Tasker on Android, for example.
 
-        Intent().also {
+        var intent = Intent()
+            intent.setAction("info.nightscout.androidaps.CONFIRM_RESULT")
+            intent.putExtra("message", message)
+            sendBroadcast(intent)
 
-            it.setAction("info.nightscout.androidaps.CONFIRM_RESULT")
-            it.putExtra("message", message)
-            sendBroadcast(it)
-        }
         aapsLogger.debug(LTag.VOICECOMMAND, String.format(resourceHelper.gs(R.string.voiceassistant_messagetouser), message))
     }
 }
