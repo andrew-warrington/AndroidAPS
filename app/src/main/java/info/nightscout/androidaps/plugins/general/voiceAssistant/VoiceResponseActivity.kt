1325b3185dfa1da2783ed98a63fd74679199d480
Andrew Warrington <awarring000@yahoo.com>
2021-02-02 12:18:51 +0000

    Testing of VoiceAssistantActivity.kt

--- app/src/main/java/info/nightscout/androidaps/plugins/general/voiceAssistant/VoiceResponseActivity.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/voiceAssistant/VoiceResponseActivity.kt
@@ -3,6 +3,7 @@ package info.nightscout.androidaps.plugins.general.voiceAssistant
 import android.content.Context
 import android.content.Intent
 import info.nightscout.androidaps.R
+import info.nightscout.androidaps.activities.MyPreferenceFragment
 import info.nightscout.androidaps.activities.NoSplashAppCompatActivity
 import info.nightscout.androidaps.logging.AAPSLogger
 import info.nightscout.androidaps.logging.LTag
@@ -11,17 +12,18 @@ import javax.inject.Inject
 class VoiceResponseActivity : NoSplashAppCompatActivity() {
 
     @Inject lateinit var aapsLogger: AAPSLogger
+    private var myIntent: Intent? = null
 
    fun messageToUser(message: String) {
 
         //external voice assistant must implement a receiver to speak these messages back to the user.
         //this is possible via Tasker on Android, for example.
 
-        Intent().also {
+        myIntent = Intent().also {
             it.setAction("info.nightscout.androidaps.CONFIRM_RESULT")
             it.putExtra("message", message)
         }
-        this.sendBroadcast(intent)
+        sendBroadcast(myIntent)
         aapsLogger.debug(LTag.VOICECOMMAND, String.format(resourceHelper.gs(R.string.voiceassistant_messagetouser), message))
     }
 }
