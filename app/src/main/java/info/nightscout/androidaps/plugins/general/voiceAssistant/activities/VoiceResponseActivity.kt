d94a5cec621a4a39ba3c976187fc6953b5991af9
Andrew Warrington <awarring000@yahoo.com>
2021-02-03 07:12:34 +0000

    Testing of VoiceAssistantActivity.kt

--- app/src/main/java/info/nightscout/androidaps/plugins/general/voiceAssistant/activities/VoiceResponseActivity.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/voiceAssistant/activities/VoiceResponseActivity.kt
@@ -3,25 +3,24 @@ package info.nightscout.androidaps.plugins.general.voiceAssistant.activities
 // Receives intents from an external voice assistant and forwards to the VoiceAssistantPlugin
 // As of Jan 2021 recommend voice assistant integration via Tasker and AutoVoice apps on Android, as these work with both Google and Alexa
 
+import android.content.Context
 import android.content.Intent
-import android.os.Bundle
 import info.nightscout.androidaps.R
 import info.nightscout.androidaps.activities.NoSplashAppCompatActivity
-import info.nightscout.androidaps.interfaces.*
 import info.nightscout.androidaps.logging.AAPSLogger
 import info.nightscout.androidaps.logging.LTag
-import info.nightscout.androidaps.plugins.general.voiceAssistant.VoiceAssistantPlugin
 import javax.inject.Inject
 
 class VoiceResponseActivity : NoSplashAppCompatActivity() {
 
     @Inject lateinit var aapsLogger: AAPSLogger
-    @Inject lateinit var activePlugin: ActivePluginProvider
 
     fun messageToUser(intent: Intent) {
 
+        var context: Context = applicationContext
+
         if (intent != null) {
-            this.applicationContext.sendBroadcast(intent)
+            context.sendBroadcast(intent)
             aapsLogger.debug(LTag.VOICECOMMAND, String.format(resourceHelper.gs(R.string.voiceassistant_messagetouser), intent.getStringExtra("message)")))
         }
     }
